ab72266eb2cc2eb6a89d6ca1bf5852ed7fc49575
pavolhodas <pavolhodas4@gmail.com>
2020-09-30 20:40:34 +0200

    IsLessThan10

--- Open-Lab-01.01/Exercise.cs
+++ Open-Lab-01.01/Exercise.cs
@@ -6,7 +6,8 @@ namespace Open_Lab_01._01
     {
         public bool IsLessThan10(string input)
         {
-            throw new NotImplementedException();
+        int myInt = ToString(input);
+         return myInt < 10;
         }
     }
 }
